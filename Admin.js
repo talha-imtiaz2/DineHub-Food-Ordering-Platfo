3c0101d93ac5a05003b5b6da4ff3f7fd97f935b7
Talha Imtiaz <talhaimtiaz336@gmail.com>
2026-09-22 18:09:33 +0500

    Create Admin model with username and password fields

---
+++ Admin.js
@@ -0,0 +1,17 @@
+// Backend/Models/Admin.js
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Admin', adminSchema);
